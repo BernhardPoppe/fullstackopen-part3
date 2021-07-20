9faf92f01b602a5f511ff67e83eda51819d8360a
Bernhard Poppe <bernhard.poppe@gmail.com>
2021-07-20 18:57:16 +0200

    e 3.14 done

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => {
     response.json(person)
@@ -44,15 +21,19 @@ app.get('/api/persons', (request, response) => {
 
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  }).catch(error => {
-            return response.status(400).json({ 
-              error: 'no entry for the id' 
-            })
-        })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+    })
 })
-
 
 app.post('/api/persons', (request, response) => {
   let data = request.body
@@ -78,10 +59,10 @@ app.post('/api/persons', (request, response) => {
 
 
 
-app.get('/info', (request, response) => {
+/*app.get('/info', (request, response) => {
   let date = new Date()
   response.send(`<p>Phonebook has info for ${persons.length} people </p><p>${date}</p>`)
-})
+})*/
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
