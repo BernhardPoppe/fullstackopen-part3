1f29fdd3c328aaf694c6c3468bd0dccdcd43970a
Bernhard Poppe <bernhard.poppe@gmail.com>
2021-07-21 10:49:36 +0200

    e 3.18 done

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => {
-    response.json(person)
+    const x = response.json(person)
   })
+  .catch(error => next(error))
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -29,40 +30,83 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    number:body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
-  let data = request.body
-  if (!data.name) {
+  let body = request.body
+  if (!body.name) {
     return response.status(400).json({ 
         error: 'name is missing' 
       })
-  } else if (!data.number) {
+  } else if (!body.number) {
     return response.status(400).json({ 
           error: 'number missing' 
       })
   }
 
   const person = new Person({
-    name: data.name,
-    number: data.number,
+    name: body.name,
+    number: body.number,
   })
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 
 
-/*app.get('/info', (request, response) => {
-  let date = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people </p><p>${date}</p>`)
-})*/
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count =>{
+      let date = new Date()
+      response.send(`<p>Phonebook has info for ${count} people </p><p>${date}</p>`)
+  })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
